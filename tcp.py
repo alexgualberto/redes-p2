469a17c6871e8f8f1a9308d55bff596dcfb9542c
Alexandre Gualberto <alexandre.santos.gualberto@gmail.com>
2024-08-13 16:28:03 -0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,8 +1,8 @@
 import asyncio
-from tcputils import *
+import math
 import random
 import time
-import math
+from tcputils import *
 
 class Servidor:
     def __init__(self, rede, porta):
@@ -13,18 +13,12 @@ class Servidor:
         self.rede.registrar_recebedor(self._rdt_rcv)
 
     def registrar_monitor_de_conexoes_aceitas(self, callback):
-        """
-        Usado pela camada de aplicação para registrar uma função para ser chamada
-        sempre que uma nova conexão for aceita
-        """
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
-        src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        src_port, dst_port, seq_no, ack_no, flags, window_size, checksum, urg_ptr = read_header(segment)
 
         if dst_port != self.porta:
-            # Ignora segmentos que não são destinados à porta do nosso servidor
             return
         if not self.rede.ignore_checksum and calc_checksum(segment, src_addr, dst_addr) != 0:
             print('descartando segmento com checksum incorreto')
@@ -34,15 +28,10 @@ class Servidor:
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-            # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
-            # TODO: talvez você precise passar mais coisas para o construtor de conexão
             conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
                 self.callback(conexao)
         elif id_conexao in self.conexoes:
-            # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
         else:
             print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
@@ -77,9 +66,6 @@ class Conexao:
         self.timer = asyncio.get_event_loop().call_later(self.timeoutInterval, self.retransmitir)
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
-        # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
-        # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
         src_addr, src_port, dst_addr, dst_port = self.id_conexao
         self.t1 = time.time()
         if (flags & FLAGS_FIN) == FLAGS_FIN:
@@ -113,24 +99,11 @@ class Conexao:
             self.callback(self, payload)
             self.ack_no += len(payload)
             self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK), dst_addr, src_addr), src_addr)
-            print('recebido payload: %r' % payload)
-
-    # Os métodos abaixo fazem parte da API
 
     def registrar_recebedor(self, callback):
-        """
-        Usado pela camada de aplicação para registrar uma função para ser chamada
-        sempre que dados forem corretamente recebidos
-        """
         self.callback = callback
 
     def enviar(self, dados):
-        """
-        Usado pela camada de aplicação para enviar dados
-        """
-        # TODO: implemente aqui o envio de dados.
-        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
-        # que você construir para a camada de rede.
         src_addr, src_port, dst_addr, dst_port = self.id_conexao
         self.unsent += dados
         for _ in range(0, self.cwnd):
@@ -145,10 +118,6 @@ class Conexao:
             self.timer = asyncio.get_event_loop().call_later(self.timeoutInterval, self.retransmitir)
 
     def fechar(self):
-        """
-        Usado pela camada de aplicação para fechar a conexão
-        """
-        # TODO: implemente aqui o fechamento de conexão
         src_addr, src_port, dst_addr, dst_port = self.id_conexao
         self.servidor.rede.enviar(fix_checksum(make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_FIN), dst_addr, src_addr), src_addr)
         self.fin = True
