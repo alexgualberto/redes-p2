588bbf7fd028c5a5e6a2b67c4985cb170a074807
Alexandre Gualberto <alexandre.santos.gualberto@gmail.com>
2024-08-13 15:16:16 -0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -28,7 +28,7 @@ class Servidor:
             checksum,
             urg_ptr,
         ) = read_header(segment)
-    
+
         if dst_port != self.porta:
             return
         if (
@@ -38,9 +38,9 @@ class Servidor:
             print("descartando segmento com checksum incorreto")
             return
 
-        payload = segment[4 * (flags >> 12) :]
+        payload = segment[4 * (flags >> 12):]
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
-    
+
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # Inicializando conexão e enviando SYN + ACK
             seq_no_svr = secrets.randbelow(65535)
@@ -58,16 +58,6 @@ class Servidor:
                 "%s:%d -> %s:%d (pacote associado a conexão desconhecida)"
                 % (src_addr, src_port, dst_addr, dst_port)
             )
-            
-    def inic_conexao(self, id_conexao, segment):
-        _, _, seq_no, _, flags, _, _, _ = read_header(segment)
-        src_addr, src_port, dst_addr, dst_port = id_conexao
-        ack_no = seq_no + 1
-        seq_no = secrets.randbelow(10)
-        seg_ack = make_header(dst_port, src_port, seq_no, ack_no, FLAGS_ACK | FLAGS_SYN)
-        seg_ack = fix_checksum(seg_ack, src_addr, dst_addr)
-        self.rede.enviar(seg_ack, src_addr)
-        return Conexao(self, id_conexao, ack_no, seq_no + 1)
 
 class Conexao:
     def __init__(self, servidor, id_conexao, ack_no, seq_no):
@@ -107,16 +97,26 @@ class Conexao:
         self.timer = None
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        if self.ack_no != seq_no:
+        if seq_no != self.ack_no:
             return
-        if (flags & FLAGS_FIN) == FLAGS_FIN and not self.closing:
-            self.closing = True 
-            self.callback(self, b"")
-            self.ack_no = self.ack_no + 1
-            self.enviar_seg_ack(b"") 
-        elif (flags & FLAGS_ACK) == FLAGS_ACK and self.closing:
+
+        self.ack_no += len(payload)
+        
+        if payload:
+            self.callback(self, payload)
+
+        # Enviar ACK para confirmar o recebimento
+        header = make_header(self.id_conexao[1], self.id_conexao[3], self.seq_no, self.ack_no, FLAGS_ACK)
+        ack_segment = fix_checksum(header, self.id_conexao[0], self.id_conexao[2])
+        self.servidor.rede.enviar(ack_segment, self.id_conexao[2])
+
+        # Tratamento de FIN flag para fechamento de conexão
+        if (flags & FLAGS_FIN) == FLAGS_FIN:
+            self.closing = True
+            self.ack_no += 1
+            self.enviar_seg_ack(b"")
             del self.servidor.conexoes[self.id_conexao]
-            return
+            self.callback(self, b"")
 
         if (flags & FLAGS_ACK) == FLAGS_ACK and ack_no > self.sendb:
             self.unacked = self.unacked[ack_no - self.sendb :]
@@ -137,12 +137,6 @@ class Conexao:
             self.byt_ack = self.byt_ack + MSS
             self.window = self.window + 1
             self.envio_pendente()
-
-        if payload:
-            self.ack_no = self.ack_no + len(payload)
-            self.callback(self, payload)
-            pac = fix_checksum(make_header(self.id_conexao[1], self.id_conexao[3], self.seq_no, self.ack_no, flags), self.id_conexao[0], self.id_conexao[2],)
-            self.servidor.rede.enviar(pac, self.id_conexao[2])
 
     def registrar_recebedor(self, callback):
         self.callback = callback
@@ -203,4 +197,4 @@ class Conexao:
         else:
             self.estimated_rtt = ((0.75) * self.estimated_rtt) + (0.25 * self.sample_rtt)
             self.devr = ((0.5) * self.devr) + (0.5 * abs(self.sample_rtt - self.estimated_rtt))
-        self.interv = self.estimated_rtt + (4 * self.devr) 
+        self.interv = self.estimated_rtt + (4 * self.devr)
